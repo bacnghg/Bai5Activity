3fe351614fbe03fe7e5a5ad85a89bdc4f57b773e
Thangcoder <trangchongchengit@gmail.com>
2016-09-18 00:29:27 +0700

    Initial commit

---
+++ app/src/main/java/com/thangcoder/bai5activity/DialogActivity.java
@@ -0,0 +1,15 @@
+package com.thangcoder.bai5activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 18/09/2016.
+ */
+public class DialogActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
